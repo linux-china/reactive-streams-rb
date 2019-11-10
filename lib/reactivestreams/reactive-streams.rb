955f5f2d682e43f1ea92ab6f60b5e2f9d1b3b7e8
linux_china <libing.chen@gmail.com>
2019-11-10 22:27:15 +0800

    reactive streams specification

---
+++ lib/reactivestreams/reactive-streams.rb
@@ -0,0 +1,38 @@
+module ReactiveStreams
+  VERSION = "0.0.1"
+
+  class Publisher
+    #@param subscriber [ReactiveStreams::Subscriber]
+    def subscribe(subscriber)
+      raise "Subscribe not implemented"
+    end
+  end
+
+  class Subscription
+
+    def request(n)
+    end
+
+    def cancel
+
+    end
+
+  end
+
+  class Subscriber
+    #@param subscription [ReactiveStreams::Subscription]
+    def on_subscribe(subscription)
+
+    end
+
+    def on_next(value)
+    end
+
+    def on_error(exception)
+    end
+
+    def on_complete
+
+    end
+  end
+end
